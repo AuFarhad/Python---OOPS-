7112e857957cc9a0977a74ec2d1011d1c145fda3
AuFarhad <aufarhad1835@gmail.com>
2026-05-06 13:08:05 +0530

    Refactor Todo class and update naming conventions

--- Todo.py
+++ Todo.py
@@ -1,34 +1,48 @@
-
 class Todo():
-    def __init__(self,title,time,todo):
+    Name="Farhads Todo list"
+    def __init__(self,title,time,description):
         self.title=title
         self.time=time
-        self.todo=todo
-         
-    def get_info(self):
-        return f"Title:{self.title}\nTime:{self.time}\n{self.todo} "
+        self.description=description
+        
+    def __str__(self):
+        return f"{self.Name}\nTitle:{self.title}\ntime:{self.time}\ndescription:{self.description}"
     
-    
-class TodoList():
+class Todolist():
     def __init__(self):
         self.todos=[]
+        self.filename="todo.txt"
         
     def add_list(self,todo):
         self.todos.append(todo)
-
-    def show_all (self):
+        
+    def show_all(self):
         for todo in self.todos:
-            print(todo.get_info())
+            print(str(todo))
+        
+    def write_file(self):
+        with open(self.filename,"w") as f:
+            for todo in self.todos:
+                f.write(str(todo) + "\n")
+        print("file created")
+        
+    def read_file(self):
+        with open(self.filename,"r") as f:
+            data=f.read()
+            print("\nFile Creation\n")
+            print(data)
             
 t1=Todo("Writing","5 pm","Completing the unheard poem,Practice Essay")
 t2=Todo("Reading","6 pm ","Completing the book (The love my life)")
 t3=Todo("Coding","7 pm - 10 pm " ,"Numpy(1-hr),Pandas(1-hr),Revision(1-hr)")
 
        
-t=TodoList()
+t=Todolist()
 
 t.add_list(t1)
 t.add_list(t2)
 t.add_list(t3)
+(t.show_all())
+t.write_file()
+t.read_file()
 
-t.show_all()
